bb66cd0441a187e5c9821fd64a81c8c81280ef0d
jhabarsingh <jhabarsinghbhati23@gmail.com>
2021-05-21 11:08:35 +0530

    Completed sentiment analysis part

--- mlModel/sentiment/script.py
+++ mlModel/sentiment/script.py
@@ -1,42 +1,72 @@
-import pandas as pd
+import pickle
 import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+from sklearn.feature_extraction.text import CountVectorizer
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.metrics import roc_auc_score
+from bs4 import BeautifulSoup
+import re
 import nltk
 from nltk.corpus import stopwords
-from sklearn.feature_extraction.text import TfidfVectorizer
-from sklearn.model_selection import train_test_split
-from sklearn import naive_bayes
-from sklearn.metrics import roc_auc_score,accuracy_score
-import pickle
-nltk.download("stopwords")
+
+train_data = pd.read_csv('./dataset/labeledTrainData.tsv.zip',  delimiter="\t", quoting=3)
+test_data = pd.read_csv('./dataset/testData.tsv.zip', delimiter = "\t", quoting= 3 )
+submission1 = pd.read_csv("./dataset/sampleSubmission.csv")
+
+train_data.head()
+
+train_data.isnull().sum().sum()
+
+train_y = np.array(train_data["sentiment"])
+
+nltk.download('stopwords')
+
+def process(review):
+   review = BeautifulSoup(review, features="lxml").get_text()
+   review = re.sub("[^a-zA-Z]",' ',review)
+   review = review.lower()
+   review = review.split()
+   swords = set(stopwords.words("english"))
+   review = [w for w in review if w not in swords]               
+   return(" ".join(review))
 
 
-dataset = pd.read_csv('./reviews.txt',sep = '\t', names =['Reviews','Comments'])
-stopset = set(stopwords.words('english'))
-
-# Tokenization
-vectorizer = TfidfVectorizer(use_idf = True,lowercase = True, strip_accents='ascii',stop_words=stopset)
-vectorizer.fit_transform(dataset.Comments)
-X = vectorizer.fit_transform(dataset.Comments)
-y = dataset.Reviews
+train_x = []
+for r in range(len(train_data["review"])):
+  train_x.append(process(train_data["review"][r]))
 
 
-filename = 'token.pkl'
-with open(filename, "wb") as wfile:
-    pickle.dump(vectorizer, wfile, protocol=pickle.HIGHEST_PROTOCOL)
+vectorizer = CountVectorizer( max_features = 5000 )
+train_x = vectorizer.fit_transform(train_x)
+train_x = train_x.toarray()
 
 
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.20, random_state=42)
+with open("token.pkl", "wb") as wfile:
+    pickle.dump(vectorizer, wfile)
+
+train_x.shape, train_y.shape
+
+test_data.head()
+
+test_data.isnull().sum().sum()
 
 
-clf = naive_bayes.MultinomialNB()
-clf.fit(X_train,y_train)
+text = ["good one", "very bad"]
+test = [process(i) for i in text]
 
-# print(X_test)
-print(accuracy_score(y_test,clf.predict(X_test))*100)
 
-clf = naive_bayes.MultinomialNB()
-clf.fit(X,y)
+test = vectorizer.transform(test)
+test = test.toarray()
 
-filename = 'sentiment.pkl'
-with open(filename, "wb") as wfile:
-    pickle.dump(clf, wfile)
+test.shape
+
+
+model = RandomForestClassifier(n_estimators = 100)
+model.fit(train_x, train_y)
+train_predict = model.predict(test)
+print(train_predict)
+
+
+with open("model.pkl", "wb") as wfile:
+    pickle.dump(model, wfile)
