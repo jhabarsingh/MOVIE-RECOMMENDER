0e8be4c7f12bdafb5394907deb10e7bcd291d11a
jhabarsingh <jhabarsinghbhati23@gmail.com>
2021-05-23 11:32:27 +0530

    Added Recommend Page

---
+++ recommenderfrontend/src/components/Loading.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="text-center"
+    style="height:100vh;width:100vh;"
+  >
+
+    <v-progress-circular
+      :size="100"
+      :width="10"
+      color="purple"
+      indeterminate
+    ></v-progress-circular>
+
+  </div>
+</template>
+
+
+<style scoped>
+.v-progress-circular {
+  margin: 1rem;
+}
+</style>
