58dd63c96b058e98f106fb48baccf1fd0995e8da
jhabarsingh <jhabarsinghbhati23@gmail.com>
2021-05-23 15:58:26 +0530

    Added Cast Detail List app

---
+++ recommenderfrontend/src/components/MovieCastList.vue
@@ -0,0 +1,34 @@
+<template>
+    <div>
+        Movie Cast List
+    </div>
+</template>
+
+
+<script>
+
+export default {
+    data() {
+        return ({
+            
+        })
+    },
+    
+    props: [
+        'movies'
+    ],
+
+    methods: {
+    },
+
+    created() {
+        if(localStorage.getItem("movie")) {
+            // Let The page Load
+        }
+        else {
+            this.$router.push("/");
+        }
+    }
+    
+}
+</script>
